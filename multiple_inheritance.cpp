bfcbd4aba952f2a61ab605c38ab2070f19c1f06c
kush-bahadur-singh <kushbahadursingh@gmail.com>
2024-10-15 10:45:12 +0530

    Create multiple_inheritance.cpp

---
+++ multiple_inheritance.cpp
@@ -0,0 +1,39 @@
+//write a c++ program using multiple inheritance
+#include <iostream>
+using namespace std;
+
+class Person {
+    char name[30];
+public:
+    void read() {
+        cout << "Enter name: " << endl;
+        cin >> name;
+    }
+    
+    void display() {
+        cout << "Name: " << name << endl;
+    }
+};
+
+class Sport {
+public:
+    int m = 50;
+    void display() {
+        cout << m << endl;
+    }
+};
+
+class Student : public Person, public Sport {
+public:
+    void display() {
+        Person::display();
+        Sport::display();
+    }
+};
+
+int main() {
+    Student st;
+    st.read();
+    st.display();
+    return 0;
+}
